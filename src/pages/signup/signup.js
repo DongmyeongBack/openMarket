fb763ba596b1bf70e3fab81219b80e3d1ee6a3e7
dongmyeongback <backdongmyeong@gmail.com>
2026-01-16 10:15:04 +0900

    회원가입후 로그인페이지이동

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -381,7 +381,7 @@ submitBtn.addEventListener("click", async (e) => {
         await join(formData, currentType);
 
         alert("회원가입이 완료되었습니다!");
-        window.location.href = "/login.html";
+        window.location.href = "/src/pages/login/index.html";
     } catch (error) {
         console.error("Signup Error:", error);
 
